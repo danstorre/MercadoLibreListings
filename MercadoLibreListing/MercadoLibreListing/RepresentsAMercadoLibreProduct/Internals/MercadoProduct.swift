f44bc846252fe15c04bf6b1de300430a60818c45
Daniel Torres <danstorre@gmail.com>
2020-06-11 01:01:37 -0300

    add MercadoLibreListingProduct

---
+++ MercadoLibreListing/MercadoLibreListing/RepresentsAMercadoLibreProduct/Internals/MercadoProduct.swift
@@ -0,0 +1,18 @@
+//
+//  MercadoProduct.swift
+//  MercadoLibreListing
+//
+//  Created by Daniel Torres on 6/11/20.
+//  Copyright © 2020 dansTeam. All rights reserved.
+//
+
+import Foundation
+
+//create model mercadolibre product.
+
+
+struct MercadoLibreListingProduct {
+    
+    
+}
+
