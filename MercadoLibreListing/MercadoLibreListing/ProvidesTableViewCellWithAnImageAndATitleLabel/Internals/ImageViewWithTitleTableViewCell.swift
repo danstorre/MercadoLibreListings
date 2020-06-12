860f07894f40e4f6d38150032548d649d2e2adc9
Daniel Torres <danstorre@gmail.com>
2020-06-12 15:26:26 -0300

    add cell

---
+++ MercadoLibreListing/MercadoLibreListing/ProvidesTableViewCellWithAnImageAndATitleLabel/Internals/ImageViewWithTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageViewWithTitleTableViewCell.swift
+//  MercadoLibreListing
+//
+//  Created by Daniel Torres on 6/12/20.
+//  Copyright © 2020 dansTeam. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewWithTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
